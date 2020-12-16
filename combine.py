708ce41e5835be0315b3eb3cf185944a87286508
Hansen Liu <hansenliu0086@gmail.com>
2020-12-17 02:17:31 +1100

    Update combine.py
    remove transparency of png before changing them into pdf

--- combine.py
+++ combine.py
@@ -5,6 +5,9 @@ import glob
 import re
 
 
+from PIL import Image
+
+
 def atoi(text):
     return int(text) if text.isdigit() else text
 
@@ -18,10 +21,33 @@ def natural_keys(text):
     return [atoi(c) for c in re.split(r'(\d+)', text)]
 
 
+def remove_transparency():
+    # Absolute path to this script
+    scriptdir = os.path.dirname(os.path.abspath(__file__))
+
+    # Walk through directory
+    for root, subfolders, files in os.walk(scriptdir):
+        for file in files:
+            try:
+                image = Image.open(os.path.join(scriptdir, root, file))
+                # If image has an alpha channel
+                if image.mode == 'RGBA':
+                    # Create a blank background image
+                    bg = Image.new('RGB', image.size, (255, 255, 255))
+                    # Paste image to background image
+                    bg.paste(image, (0, 0), image)
+                    # Save pasted image as image
+                    bg.save(os.path.join(scriptdir, root, file), "PNG")
+
+            except:
+                pass
+
+
+remove_transparency()
 imagelist = glob.glob("*.jpg")
 
+#folderlist = glob.glob('室友*')
 folderlist = os.listdir(os.getcwd())
-print(folderlist)
 folderlist.sort(key=natural_keys)
 # print(folderlist)
 temp = []
@@ -31,5 +57,7 @@ with open("output.pdf", "wb") as f:
         currjpgs = glob.glob(folder+"/"+"*.jpg")
         for i in currjpgs:
             imagelist.append(i)
-    #imagelist = imagelist[:1200]
+    # imagelist = imagelist[]
+    # print(imagelist[-1])
+
     f.write(img2pdf.convert([i for i in imagelist if i.endswith(".jpg")]))
